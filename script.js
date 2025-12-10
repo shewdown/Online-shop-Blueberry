612dfbe334a9fb4d8e195d5e186aa7d71375b2a4
KirillLaptop <kfisukov@gmail.com>
2025-12-10 17:51:49 +0700

    v2.2

--- script.js
+++ script.js
@@ -14,6 +14,28 @@ function showNotification(message, duration = 3000) {
     }, duration);
 }
 
+function openSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.add('show');
+    openOverlay();
+}
+
+function closeSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.remove('show');
+    closeOverlay();
+}
+
+function openOverlay() {
+    const overlay = document.querySelector('.overlay-sidebar');
+    overlay.classList.add('show');
+}
+
+function closeOverlay() {
+    const overlay = document.querySelector('.overlay-sidebar');
+    overlay.classList.remove('show');
+}
+
 document.addEventListener("DOMContentLoaded", () => {
 
     // ============================================================
